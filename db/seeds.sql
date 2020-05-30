a7f65274588a076540784203f7d19d9d32965d1f
Azucena Herrera <azucenaphx@gmail.com>
2020-05-29 19:41:30 -0700

    completed sql files and connection

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burgers_db;
+INSERT INTO burgers (burger_name, Devoured) VALUES ('Southwestern Burger', true);
+INSERT INTO burgers (burger_name, Devoured) VALUES ('BBQ Bacon Cheddar Burger', true); 
+INSERT INTO burgers (burger_name, Devoured) VALUES ('Cheessburger', true);
+INSERT INTO burgers (burger_name, Devoured) VALUES ('Mushroom and Swiss Burger', true);
+INSERT INTO burgers (burger_name, Devoured) VALUES ('Guacamole Bacon Cheeseburger', true);
+INSERT INTO burgers (burger_name, Devoured) VALUES ('Buffalo Chicken Burger', true);
