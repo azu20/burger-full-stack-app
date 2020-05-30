5cc924578ce4b84a9b22c09666a11eca6297ee73
Azucena Herrera <azucenaphx@gmail.com>
2020-05-29 19:42:57 -0700

    added orm file

--- controllers/orm.js
+++ controllers/orm.js
@@ -15,17 +15,16 @@ const printQuestionMarks = num => {
 const objToSql = ob => {
   const arr = [];
 
-  // loop through the keys and push the key/value as a string int arr
+
   for (const key in ob) {
     let value = ob[key];
-    // check to skip hidden properties
+
     if (Object.hasOwnProperty.call(ob, key)) {
-      // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+
       if (typeof value === "string" && value.indexOf(" ") >= 0) {
         value = "'" + value + "'";
       }
-      // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
-      // e.g. {sleepy: true} => ["sleepy=true"]
+
       arr.push(key + "=" + value);
     }
   }
