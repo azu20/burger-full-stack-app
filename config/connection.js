4dfbf41aeaf2308c91ccc48c885bb8462b71e2ac
Azucena Herrera <azucenaphx@gmail.com>
2020-06-02 18:27:00 -0700

    heroku DB set up

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,21 @@
 const mysql = require("mysql");
 const util = require("util");
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "New2020ya$!",
-    database: "burgers_db"
-});
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "New2020ya$!",
+        database: "burgers_db"
+    });
+};
+
+
 
 // Make connection.
 connection.connect((err) => {
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,9 +1,8 @@
 USE burgers_db;
 INSERT INTO burger (burger_name, Devoured) VALUES ('Southwestern Burger', true); 
-INSERT INTO burger (burger_name, Devoured) VALUES ('BBQ Bacon Cheddar Burger', False); 
--- INSERT INTO burger (burger_name, Devoured) VALUES ('Cheeeseburger', true);
--- INSERT INTO burger (burger_name, Devoured) VALUES ('Mushroom and Swiss Burger', true);
--- INSERT INTO burger (burger_name, Devoured) VALUES ('Guacamole Bacon Cheeseburger', true);
--- INSERT INTO burger (burger_name, Devoured) VALUES ('Buffalo Chicken Burger', true);
+INSERT INTO burger (burger_name, Devoured)--  VALUES ('Cheeeseburger', true);
+INSERT INTOburger burger (burger_name, Devoured) VALUES ('Mushroom and Swiss Burger', true);
+INSERT INTO burger (burger_name, Devoured) VALUES ('Guacamole Bacon Cheeseburger', false);
+INSERT INTO burger (burger_name, Devoured) VALUES ('Buffalo Chicken Burger', false);
 
 select * from burger;
